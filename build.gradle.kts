b566c8bd0b9507d16378919b29c4d610bae85bc1
Arthac <121276031+Arthac@users.noreply.github.com>
2025-02-09 14:56:29 +1000

    Normalize line endings to LF

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.serialization") version "1.9.23"
-    id("maven-publish")
+    `maven-publish`
 }
+
 group = "com.github.sanctus-corvus"
-version = "0.1"
+version = "0.1.0"
+
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17)) // 17-й JDK стабилен для JitPack
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
+
 repositories {
     mavenCentral()
 }
@@ -20,51 +23,38 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
-kotlin {
-    jvmToolchain(11)
-}
-tasks.register<Jar>("sourcesJar") {
-    archiveClassifier.set("sources")
-    from(sourceSets.main.get().allSource)
-}
 
-tasks.register<Jar>("javadocJar") {
-    archiveClassifier.set("javadoc")
-    from(tasks.getByName("javadoc"))
+kotlin {
+    explicitApi()
 }
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = "lumina-kt"
-            version = project.version.toString()
-
             from(components["java"])
-            artifact(tasks.getByName("sourcesJar"))
-            artifact(tasks.getByName("javadocJar"))
 
             pom {
-                name.set("LuminaKt")
-                description.set("Kotlin client library for the Gemini API")
-                url.set("https://github.com/sanctus-corvus/LuminaKt")
+                name = "Gemini Kotlin Client"
+                description = "Kotlin client library for the Gemini API"
+                url = "https://github.com/sanctus-corvus/LuminaKt"
                 licenses {
                     license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
+                        name = "MIT License"
+                        url = "https://opensource.org/licenses/MIT"
                     }
                 }
                 developers {
                     developer {
-                        id.set("sanctus-corvus")
-                        name.set("Sanctus Corvus")
+                        id = "sanctus-corvus"
+                        name = "Sanctus Corvus"
                     }
                 }
                 scm {
-                    url.set("https://github.com/sanctus-corvus/LuminaKt")
+                    connection = "scm:git:git://github.com/sanctus-corvus/LuminaKt.git"
+                    developerConnection = "scm:git:ssh://github.com:sanctus-corvus/LuminaKt.git"
+                    url = "https://github.com/sanctus-corvus/LuminaKt"
                 }
             }
         }
     }
 }
-
